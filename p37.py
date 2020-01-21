f3d0a79910951f8994fe01f17757691e76d62463
Rohit Bhati <Rbhati@bestpeers.com>
2020-01-21 13:23:57 +0530

    Solution of question-37

---
+++ p37.py
@@ -0,0 +1,23 @@
+"""
+Question 37:
+Define a function which can generate and print a list where the values are square of numbers
+between 1 and 20 (both included).
+"""
+input_num = input("Write number:")
+lis1=[]
+
+def lis(n1,n2):
+	"""
+	Generate and print list where the values are square of numbers.
+	param:n1,n2
+	"""
+	if n1 and n2 <= 20:
+		for x in range(n1,n2+1):
+			lis1.append(x*x)
+		print(lis1)
+		
+	else:
+		print("Value out of range")
+
+# Calling function.
+lis(input_num[0],input_num[1])
